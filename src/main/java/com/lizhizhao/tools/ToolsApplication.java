a54d538e1b0acc724d6009f77309291956aefd90
李志钊 <lizhizhaooo@163.com>
2020-05-17 14:11:43 +0800

    first commit

---
+++ src/main/java/com/lizhizhao/tools/ToolsApplication.java
@@ -0,0 +1,11 @@
+package com.lizhizhao.tools;
+
+
+@SpringBootApplication
+public class ToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsApplication.class, args);
+    }
+
+}
