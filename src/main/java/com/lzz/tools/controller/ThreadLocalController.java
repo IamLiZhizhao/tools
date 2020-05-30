a29b669636a3fd2bfa4b50965a49c916890d4b94
李志钊 <lizhizhaooo@163.com>
2020-05-30 18:35:38 +0800

    [业务错误代码测试]

---
+++ src/main/java/com/lzz/tools/controller/ThreadLocalController.java
@@ -0,0 +1,57 @@
+package com.lzz.tools.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 验证线程重用导致用户信息错乱的 Bug
+ * @author lizhizhao
+ * @since 2020-05-30 16:15
+ */
+@RestController
+@RequestMapping("threadLocal")
+public class ThreadLocalController {
+
+    private static final ThreadLocal<Integer> currentUser = ThreadLocal.withInitial(() -> null);
+
+    //顾名思义，线程池会重用固定的几个线程，一旦线程重用，那么很可能首次从 ThreadLocal 获取的值是之前其他用户的请求遗留的值。
+    // 这时，ThreadLocal 中的用户信息就是其他用户的信息。
+    @GetMapping("wrong")
+    public Map wrong(@RequestParam("userId") Integer userId) {
+
+        //设置用户信息之前先查询一次ThreadLocal中的用户信息
+        String before  = Thread.currentThread().getName() + ":" + currentUser.get();
+        //设置用户信息到ThreadLocal
+        currentUser.set(userId);
+        //设置用户信息之后再查询一次ThreadLocal中的用户信息
+        String after  = Thread.currentThread().getName() + ":" + currentUser.get();
+        //汇总输出两次查询结果
+        Map result = new HashMap();
+        result.put("before", before);
+        result.put("after", after);
+        return result;
+    }
+
+
+    @GetMapping("right")
+    public Map right(@RequestParam("userId") Integer userId) {
+        String before  = Thread.currentThread().getName() + ":" + currentUser.get();
+        currentUser.set(userId);
+        try {
+            String after = Thread.currentThread().getName() + ":" + currentUser.get();
+            Map result = new HashMap();
+            result.put("before", before);
+            result.put("after", after);
+            return result;
+        } finally {
+            //在finally代码块中删除ThreadLocal中的数据，确保数据不串
+            currentUser.remove();
+        }
+    }
+
+}
